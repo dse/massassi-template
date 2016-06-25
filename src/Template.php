f590c91c029f4082fad96f09bd01abdf1d200850
Darren Embry <dse@webonastick.com>
2016-06-24 23:03:54 -0400

    remove the emacs stuff.
    re-indent.

--- src/Template.php
+++ src/Template.php
@@ -1,47 +1,47 @@
 <?php
+/**
+ * This is a native PHP templating system written in 50 lines of PHP.
+ *
+ * Maintained by Darren Embry <dse@webonastick.com>
+ * https://github.com/dse/massassi-template/
+ *
+ * See also: http://www.massassi.com/php/articles/template_engines/
+ *
+ * This code is in the public domain.
+ */
+class Template {
+    var $vars; /// Holds all the template variables
+
     /**
-     * This is a native PHP templating system written in 50 lines of PHP.
+     * Constructor
      *
-     * Maintained by Darren Embry <dse@webonastick.com>
-     * https://github.com/dse/massassi-template/
-     *
-     * See also: http://www.massassi.com/php/articles/template_engines/
-     *
-     * This code is in the public domain.
+     * @param $file string the file name you want to load
      */
-    class Template {
-        var $vars; /// Holds all the template variables
-
-        /**
-         * Constructor
-         *
-         * @param $file string the file name you want to load
-         */
-        function Template($file = null) {
-            $this->file = $file;
-        }
-
-        /**
-         * Set a template variable.
-         */
-        function set($name, $value) {
-            $this->vars[$name] = is_a($value, "Template") ? $value->fetch() : $value;
-        }
-
-        /**
-         * Open, parse, and return the template file.
-         *
-         * @param $file string the template file name
-         */
-        function fetch($file = null) {
-            if(!$file) $file = $this->file;
-
-            extract($this->vars);          // Extract the vars to local namespace
-            ob_start();                    // Start output buffering
-            include($file);                // Include the file
-            $contents = ob_get_contents(); // Get the contents of the buffer
-            ob_end_clean();                // End buffering and discard
-            return $contents;              // Return the contents
-        }
+    function Template($file = null) {
+        $this->file = $file;
     }
+
+    /**
+     * Set a template variable.
+     */
+    function set($name, $value) {
+        $this->vars[$name] = is_a($value, "Template") ? $value->fetch() : $value;
+    }
+
+    /**
+     * Open, parse, and return the template file.
+     *
+     * @param $file string the template file name
+     */
+    function fetch($file = null) {
+        if(!$file) $file = $this->file;
+
+        extract($this->vars);          // Extract the vars to local namespace
+        ob_start();                    // Start output buffering
+        include($file);                // Include the file
+        $contents = ob_get_contents(); // Get the contents of the buffer
+        ob_end_clean();                // End buffering and discard
+        return $contents;              // Return the contents
+    }
+}
 ?>
